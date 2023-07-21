31c3ce5c07ef20a2956eee27c75e71c76d935d72
ericwalkerw <ericwalker.leo@gmail.com>
2023-07-21 11:25:46 +0700

    add_setting_popup

---
+++ Day1/Assets/Scripts/Setting.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Setting : ScriptableObject
+{
+    public float sfxVolume;
+    public float MusicVolume;
+    public int GraphicIndex;
+    public int LanguageIndex;
+}
---
+++ Day1/Assets/Scripts/SettingUI.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingUI : MonoBehaviour
+{
+    public Slider sfxSlider;
+    public Slider musicSlider;
+    public TMP_Dropdown graphicDropdown;
+    public TMP_Dropdown languageDropdown;
+    public Setting settingData;
+
+    private void Start()
+    {
+        LoadSettingData();
+    }
+
+    private void LoadSettingData()
+    {
+        sfxSlider.value = settingData.sfxVolume;
+        musicSlider.value = settingData.MusicVolume;
+        graphicDropdown.value = settingData.GraphicIndex;
+        languageDropdown.value = settingData.LanguageIndex;
+    }
+
+    public void OnSfxVolumeChanged(float value) => settingData.sfxVolume = value;
+    public void OnMusicVolumeChanged(float value) => settingData.MusicVolume = value;
+    public void OnGraphicIndexChanged(int index) => settingData.GraphicIndex = index;
+    public void OnLanguageIndexChanged(int index) => settingData.LanguageIndex = index;
+    public void SaveSettingData()
+    {
+        settingData.sfxVolume = sfxSlider.value;
+        settingData.MusicVolume = musicSlider.value;
+        settingData.GraphicIndex = graphicDropdown.value;
+        settingData.LanguageIndex = languageDropdown.value;
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveSettingData();
+    }
+
+}
